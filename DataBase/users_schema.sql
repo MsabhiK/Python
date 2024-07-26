9dd678886ace96e4560e73e1667a579ff45a8dd6
MsabhiK <151369797+MsabhiK@users.noreply.github.com>
2024-07-26 21:45:55 +0100

    Add files via upload

---
+++ DataBase/users_schema.sql
@@ -0,0 +1,13 @@
+USE users_schema;
+/*INSERT INTO users (first_name, last_name, email) 
+VALUES ('Karim', 'MSABHI', 'sirmsabhi@gmail.com'), ('Hela', 'MSABHI', 'helamsabhi@gmail.com'), ('Mounir', 'MSABHI', 'mounirmsabhi@gmail.com');
+SELECT * FROM users;*/
+/*SELECT * FROM users WHERE email="sirmsabhi@gmail.com";  */
+/*SELECT * FROM users WHERE id=3;*/
+/*UPDATE users SET last_name='PANCAKES' WHERE id=3;
+SELECT * FROM users;*/
+/*DELETE FROM users WHERE id=2;
+SELECT * FROM users;*/
+/*SELECT last_name FROM users;*/
+/*SELECT * FROM users
+order by last_name desc;*/
