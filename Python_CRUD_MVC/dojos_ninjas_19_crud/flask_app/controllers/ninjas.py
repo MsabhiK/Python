1e7f17295980f0e1bfb93eee3497be58d0716143
MsabhiK <151369797+MsabhiK@users.noreply.github.com>
2024-08-29 18:28:06 +0100

    Add files via upload

---
+++ Python_CRUD_MVC/dojos_ninjas_19_crud/flask_app/controllers/ninjas.py
@@ -0,0 +1,20 @@
+from flask import render_template, redirect, request
+from flask_app import app
+from flask_app.models import dojo, ninja
+from flask_app.models.ninja import Ninja
+
+
+@app.route('/ninjas')
+def ninjas():
+    
+    return render_template("ninja.html", dojos= dojo.Dojo.get_all())
+
+@app.route('/create/ninja', methods=['POST'])
+def create_ninja():
+    ninja.Ninja.save(request.form)
+    return redirect('/')
+
+@app.route('/ninjas/edit/<ninja_id>')
+def edit_nija_id(ninja_id):
+    print("A modifier Ninja: ", ninja_id)
+    render_template("edit_ninja_id.html")
