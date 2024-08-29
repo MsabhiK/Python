4b64288bdc0da393ddffe66719061d007b7d149a
MsabhiK <151369797+MsabhiK@users.noreply.github.com>
2024-08-29 21:28:29 +0100

    Update ninjas.py

--- Python_CRUD_MVC/dojos_ninjas_19_crud/flask_app/controllers/ninjas.py
+++ Python_CRUD_MVC/dojos_ninjas_19_crud/flask_app/controllers/ninjas.py
@@ -1,7 +1,6 @@
 from flask import render_template, redirect, request
 from flask_app import app
 from flask_app.models import dojo, ninja
-from flask_app.models.ninja import Ninja
 
 
 @app.route('/ninjas')
